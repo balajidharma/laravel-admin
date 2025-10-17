0f17e30e8abc11d8a246407891f35bf8a4ca260c
balajidharma <balajidharma@hotmail.com>
2025-10-16 21:52:29 -0400

    Added laravel-admin:install command

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -2,31 +2,45 @@
 
 namespace BalajiDharma\LaravelAdmin;
 
+use BalajiDharma\LaravelAdmin\AdminAuthServiceProvider;
 use BalajiDharma\LaravelAdmin\Composers\MenuComposer;
-use Symfony\Component\Console\Output\ConsoleOutput;
+use BalajiDharma\LaravelAdmin\Commands\InstallCommand;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AdminServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(AdminAuthServiceProvider::class);
+    }
+
     public function boot()
     {
         View::composer('laravel-admin::layouts.navigation', MenuComposer::class);
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-admin');
         $this->registerRoutes();
 
-        if (app()->runningInConsole()) {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InstallCommand::class,
+            ]);
             $this->publishes([
                 __DIR__ . '/../resources/js' => resource_path('js/vendor/laravel-admin'),
                 __DIR__ . '/../resources/css' => resource_path('css/vendor/laravel-admin'),
                 __DIR__ . '/../resources/views' => resource_path('views/vendor/laravel-admin'),
+                __DIR__ . '/../stubs/package.json' => base_path('package.json'),
                 __DIR__ . '/../tailwind.admin.config.js' => base_path('tailwind.admin.config.js'),
+                __DIR__ . '/../stubs/Models/Permission.php.stub' => base_path('Models/Permission.php'),
+                __DIR__ . '/../stubs/Models/Role.php.stub' => base_path('Models/Role.php'),
+                __DIR__ . '/../stubs/Models/User.php.stub' => base_path('Models/User.php'),
             ], ['laravel-admin-resources']);
-
-            // Check if the main app's vite config includes our paths
-            $this->checkViteConfig();
         }
     }
 
@@ -39,23 +53,5 @@ class AdminServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
              ->group(__DIR__.'/../routes/admin.php');
-    }
-
-    protected function checkViteConfig()
-    {
-        $viteConfigPath = base_path('vite.config.js');
-        $viteConfigTsPath = base_path('vite.config.ts');
-
-        if (File::exists($viteConfigPath) || File::exists($viteConfigTsPath)) {
-            $output = new ConsoleOutput();
-            $output->writeln('Please make sure your Vite config includes:');
-            $output->writeln('input: [');
-            $output->writeln('    "resources/css/app.css",');
-            $output->writeln('    "resources/js/app.js",');
-            $output->writeln('    "resources/js/vendor/laravel-admin/app.js",');
-            $output->writeln('    "resources/js/vendor/laravel-admin/form-builder/field.js",');
-            $output->writeln('    "resources/css/vendor/laravel-admin/app.scss"');
-            $output->writeln('],');
-        }
     }
 }
---
+++ src/Commands/InstallCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace BalajiDharma\LaravelAdmin\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class InstallCommand extends Command
+{
+    protected $signature = 'laravel-admin:install
+                            {--force : Force the installation}';
+
+    protected $description = 'Install the Laravel Admin package';
+
+    public function handle()
+    {
+        $force = $this->option('force') ?? false;
+        if ($force) {
+            $this->info('Forcing installation...');
+        }
+        $this->info('Installing Laravel Admin...');
+
+        $this->info('Publishing assets...');
+        $this->call('vendor:publish', ['--tag' => 'laravel-admin-resources'] + ($force ? ['--force' => true] : []));
+        $this->call('vendor:publish', ['--tag' => 'admin-core'] + ($force ? ['--force' => true] : []));
+
+        if ($this->confirm('Do you want to run migrations and seed the database?')) {
+            $this->info('Running migrations and seeding...');
+            $this->call('migrate', [
+                '--seed' => true,
+                '--seeder' => 'AdminCoreSeeder',
+                '--force' => $force,
+            ]);
+        }
+
+        $this->info('Linking storage...');
+        $this->call('storage:link');
+
+        $this->checkViteConfig();
+
+        $this->info('Laravel Admin installed successfully.');
+    }
+
+    protected function checkViteConfig()
+    {
+        $viteConfigPath = base_path('vite.config.js');
+        $viteConfigTsPath = base_path('vite.config.ts');
+
+        if (File::exists($viteConfigPath) || File::exists($viteConfigTsPath)) {
+            $this->info('Please make sure your Vite config includes:');
+            $this->info('input: [');
+            $this->info('    "resources/css/app.css",');
+            $this->info('    "resources/js/app.js",');
+            $this->info('    "resources/js/vendor/laravel-admin/app.js",');
+            $this->info('    "resources/js/vendor/laravel-admin/form-builder/field.js",');
+            $this->info('    "resources/css/vendor/laravel-admin/app.scss",');
+            $this->info('],');
+        }
+    }
+}
